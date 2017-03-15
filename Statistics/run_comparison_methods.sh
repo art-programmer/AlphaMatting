9465c2c6ec1f38671b1c730b8df47213b1f6518e
erikwijmans <ewijmans2@gmail.com>
2017-03-15 14:35:50 -0500

    Moved to harder problem with 2 threads

--- Statistics/run_comparison_methods.sh
+++ Statistics/run_comparison_methods.sh
@@ -13,25 +13,25 @@ ${exe} -img_name=${data_set} -num_threads=1 -num_proposals=1 -exchange_amount=0 
 cp ${data_file} fusion_move.csv
 
 echo "Running parallel fusion move"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=1 -exchange_amount=0 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=1 -exchange_amount=0 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
 cp ${data_file} parallel_fusion_move.csv
 
-echo "Running SS-MF"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=1 -exchange_amount=1 -exchange_interval=5 -timeout=${timeout_time} -resize_factor=${resize_factor}
-cp ${data_file} ss-mf.csv
+echo "Running SF-MF"
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=1 -exchange_amount=1 -exchange_interval=3 -timeout=${timeout_time} -resize_factor=${resize_factor}
+cp ${data_file} sf-mf.csv
 
 echo "Running SF-SS"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=3 -exchange_amount=0 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=3 -exchange_amount=0 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
 cp ${data_file} sf-ss.csv
 
-echo "Running SS"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=3 -exchange_amount=3 -exchange_interval=5 -timeout=${timeout_time} -resize_factor=${resize_factor}
+echo "Running SF"
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=3 -exchange_amount=2 -exchange_interval=3 -timeout=${timeout_time} -resize_factor=${resize_factor}
 cp ${data_file} sf.csv
 
 echo "Running SS-Baeysian"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=3 -exchange_amount=2 -exchange_interval=5 -timeout=${timeout_time} -resize_factor=${resize_factor}
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=2 -exchange_amount=1 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
 cp ${data_file} bopt.csv
 
 echo "Running SF-Grid"
-${exe} -img_name=${data_set} -num_threads=4 -num_proposals=2 -exchange_amount=2 -exchange_interval=4 -timeout=${timeout_time} -resize_factor=${resize_factor}
+${exe} -img_name=${data_set} -num_threads=2 -num_proposals=1 -exchange_amount=1 -exchange_interval=4 -timeout=${timeout_time} -resize_factor=${resize_factor}
 cp ${data_file} grid.csv
