1998afd4f16faa14b54deae004aa9ff3c16f1ea5
erikwijmans <ewijmans2@gmail.com>
2017-03-16 21:35:48 -0500

    Averaging

--- Statistics/run_comparison_methods.sh
+++ Statistics/run_comparison_methods.sh
@@ -2,11 +2,24 @@
 
 exe=/home/erik/Projects/ParallelFusion/build/AlphaMatting/AlphaMatting
 data_set=GT24
-timeout_time=480
+timeout_time=300
 resize_factor=-1.0
 
 data_file=raw-time-v-energy.csv
 
+
+run_test() {
+  run_name=$1
+  alpha=$2
+  beta=$3
+  gamma=$4
+
+  for i in {1..5}; do
+    ${exe} -img_name=${data_set} -num_threads=2 -num_proposals=${alpha} -exchange_amount=${beta} -exchange_interval=${gamma} -timeout=${timeout_time} -resize_factor=${resize_factor}
+    cp ${data_file} "${run_name}_${i}.csv"
+  done
+
+}
 
 # echo "Running fusion move"
 # ${exe} -img_name=${data_set} -num_threads=1 -num_proposals=1 -exchange_amount=0 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
@@ -28,10 +41,8 @@ data_file=raw-time-v-energy.csv
 # ${exe} -img_name=${data_set} -num_threads=2 -num_proposals=3 -exchange_amount=1 -exchange_interval=3 -timeout=${timeout_time} -resize_factor=${resize_factor}
 # cp ${data_file} sf.csv
 
-# echo "Running SF-Baeysian"
-# ${exe} -img_name=${data_set} -num_threads=2 -num_proposals=2 -exchange_amount=1 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
-# cp ${data_file} bopt.csv
+echo "Running SF-Baeysian"
+run_test "bopt" 2 1 1
 
 echo "Running SF-Grid"
-${exe} -img_name=${data_set} -num_threads=2 -num_proposals=2 -exchange_amount=1 -exchange_interval=1 -timeout=${timeout_time} -resize_factor=${resize_factor}
-cp ${data_file} grid.csv
+run_test "grid" 2 1 1
